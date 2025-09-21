a90ab4540044e586c68fc879847426303c0ffafe
cheznu <cheenuxiong20@gmail.com>
2025-09-20 22:08:26 -0500

    got the bot's choice coding  done

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World!!");
+function RandomInt(){
+    return Math.floor(Math.random()*3+1);
+}
+
+let computerChoice = RandomInt();
+console.log(computerChoice);
+
+if(computerChoice == 1){
+    console.log("Rock")
+}else if(computerChoice==2){
+    console.log("Scissor")
+}else if(computerChoice==3){
+    console.log("Paper")
+}
+
